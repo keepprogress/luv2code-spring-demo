deae73c2d9678c01fa474ed7499f2858e234de91
getzch <keepprogress@gmail.com>
2020-11-14 17:52:16 +0800

    Define a new implementation for the Coach interface. Run application to verify my new Coach implmentation. Change the configuration then the HelloSpringApp succesfully run with TennisCoach

---
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Hit the ball as fast as tabletennis can do";
+	}
+
+}
